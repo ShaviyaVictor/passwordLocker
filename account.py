428c0e521e86a31dd695c69b3e56b96fb909106f
ShaviyaVictor <shaviyavictor@gmail.com>
2022-01-22 12:41:42 +0300

    Update the README.md file

---
+++ account.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+class Account :
+  '''
+  Class Account that generates new instances of contacts
+
+  Use pass keyword to allow creation of empty block of code so as to return a null or nothing when it runs
+  '''
+
+  pass
+
+
+
+  
+
