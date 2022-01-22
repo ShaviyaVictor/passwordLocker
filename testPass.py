3cdd3a2af0599dddb18d111e5282ff957dde1ca4
ShaviyaVictor <shaviyavictor@gmail.com>
2022-01-22 17:11:59 +0300

    Create a subclass that inherits from its parent class unittest.TestCase

--- testPass.py
+++ testPass.py
@@ -4,4 +4,10 @@ import unittest
 # Import the Account class
 from account import Account
 
+class testAccount(unittest.TestCase) :
+  '''
+  Test class that defines test cases for the Account class behaviours
 
+  Args:
+    unittest.TestCase: TestCase class that helps in creating test cases
+  '''
