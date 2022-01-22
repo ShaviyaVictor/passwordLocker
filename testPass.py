193e8d4b571dcd18fb4554c3630c391c3c7d0143
ShaviyaVictor <shaviyavictor@gmail.com>
2022-01-22 17:21:24 +0300

    Create an instance to be run by the setUp() before the tests

--- testPass.py
+++ testPass.py
@@ -6,8 +6,20 @@ from account import Account
 
 class testAccount(unittest.TestCase) :
   '''
-  Test class that defines test cases for the Account class behaviours
+  Create a subclass test class that defines test cases for the Account class behaviours
 
   Args:
     unittest.TestCase: TestCase class that helps in creating test cases
   '''
+
+
+  def setUp(self) :
+    '''
+    setUp() method to run before each test case
+
+    Create new account object to facilitate the pre-tests
+    '''
+    self.newAccount = Account('Instagram', 'ignionreads', '33176819')
+
+  
+  
